29d3d53191b43e554306c3f1fa4278fca37e2a9e
ben <ben.transinh@gmail.com>
2022-04-29 21:27:48 +0700

    commit lesson5

---
+++ lesson5.py
@@ -0,0 +1,32 @@
+my_string = 'Hello World'
+my_name = "I am is Sinh \"I am is developer\""
+print(my_string,my_name)
+char = my_string[0] #H
+char = my_string[1] #e
+
+
+substring = my_string[1:5:1]
+print(substring)
+
+greeting = "Hello, "
+name = 'Tom'
+sentence = greeting + " " + name
+print(sentence)
+
+
+
+
+my_list = ['How', 'are', 'you']
+a = '          '.join(my_list)
+print(my_list)
+
+
+
+
+
+
+
+
+
+
+print('I am a alone'.strip('sinh'))

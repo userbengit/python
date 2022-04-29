2e730c68047d56106a249893c5445892f2e63a6a
ben <ben.transinh@gmail.com>
2022-04-29 20:39:05 +0700

    This is a new commit for what I originally planned to be amended

---
+++ lesson4_Data_Type_basic.py
@@ -0,0 +1,44 @@
+# # các kiểu dữ liệu cơ bản trong python : bool , int ,float, str, none
+# var_bool = True
+# var_false = False 
+# print(type(var_bool))
+# print(type(var_false))
+# a = var_bool + 3
+# b = var_false +8
+# print(a)
+# print(b)
+
+
+
+var_none = None
+print(type(var_none))
+email_address = None
+email_address = "ben.transinh@gmail.com"
+
+if email_address:
+    print(f"Email address is {email_address}")
+else:
+    print(f"Email address is empty or {email_address}")
+print(type(1))
+print(type(3.3))
+print(type(0.0))
+print(type(1E5))
+print(type(10+3))
+print(type(1-4))
+print(type(10*3))
+print(type(10**4))
+print(type(10/3))
+print(type(10//3))
+print(type(10%3))
+#basic function 
+
+print(pow(10, 3))
+print(round(6.68))
+print(round(7.364238))
+
+print(hex(7))
+
+
+
+
+
